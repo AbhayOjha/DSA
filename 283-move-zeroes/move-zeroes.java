fce3555bd67a1041fe05ac43a2b3c1271ff3d122
Abhay Ojha <72141222+AbhayOjha@users.noreply.github.com>
2026-09-03 00:28:45 +0530

    Time: 2 ms (91.84%) | Memory: 47.8 MB (57.59%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,17 +3,16 @@ class Solution {
         int i=0;
         int n = nums.length;
         for(int j=0; j<n; j++){
-            while(i<n && nums[i] != 0){
+            if(nums[j] != 0){
+                if(i>=j){
+                    i++;
+                    continue;
+                }
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
             }
-            while(j<n && nums[j] == 0){
-                j++;
-            }
-            if(i<n && j<n && i<j){
-                nums[i] = nums[j];
-                nums[j] = 0;
-            }
-
         }
     }
 }
