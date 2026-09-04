6e84b9ce57186218df743352af39f575cf3e75b6
Abhay Ojha <72141222+AbhayOjha@users.noreply.github.com>
2026-09-05 01:31:34 +0530

    Time: 30 ms (87.56%) | Memory: 59.1 MB (74.49%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,40 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
 
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i] == nums[i-1]) // if same as previous to avoid duplicate
-                continue;
-            
-            if(nums[i] > 0)     // if first number is positive them further will also be positive then can't be 0
-                break;
 
+        for(int i = 0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            if(nums[i] > 0)
+                break;
             int left = i+1;
             int right = n-1;
             int target = -nums[i];
             while(left<right){
-                int sum = nums[left]+nums[right];
+                int sum = nums[left] + nums[right];
                 if(sum == target){
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while(left<right && nums[left] == nums[left+1]){     //to avoid duplicate
-                        left++;
-                    }
-                    while(left<right && nums[right] == nums[right-1]){     //to avoid duplicate
+                    while(left < right && nums[right] == nums[right-1]){
                         right--;
                     }
+                    while(left < right && nums[left] == nums[left+1]){
+                        left++;
+                    }
                     left++;
                     right--;
                 }
-                else if(sum > target){
-                    right--;
+                else if(sum < target){
+                    left++;
                 }
                 else{
-                    left++;
-                }
+                    right--;
+                }  
             }
         }
         return res;
+        
     }
 }
