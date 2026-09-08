035f9f99c2d20d8f9e948f99a96bd2089ae52fac
Abhay Ojha <72141222+AbhayOjha@users.noreply.github.com>
2026-09-09 02:39:58 +0530

    Time: 7 ms (50.91%) | Memory: 51.2 MB (41.93%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,25 +1,28 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int minSum = Integer.MAX_VALUE;
-        int maxSum = Integer.MIN_VALUE;
-        int total = 0;
-        int curMax = 0;
-        int curMin = 0;
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int curMin = nums[0];
+        int curMax = nums[0];
+        int total = nums[0];
 
-        for(int i =0; i<nums.length; i++){
-            // Kadane for max subarray
+        for(int i=1; i<nums.length; i++){
+            //kadane for max
             curMax = Math.max(nums[i], curMax+nums[i]);
             maxSum = Math.max(maxSum, curMax);
 
-            // Kadane for min subarray
+            //kadane for min
             curMin = Math.min(nums[i], curMin+nums[i]);
             minSum = Math.min(minSum, curMin);
 
             total += nums[i];
         }
-        if(maxSum < 0){     // if all elements are negative then total - minSum will 0 idicating false empty array so for that we return max sum as per plain Kadane
+
+        if(maxSum < 0){
             return maxSum;
         }
-        return Math.max(maxSum, total - minSum);
-    }      //Plain Kadane -|        |- wrap around minimum subarray
+
+        return Math.max(maxSum, total-minSum);
+
+    }
 }
